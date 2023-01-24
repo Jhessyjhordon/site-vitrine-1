227c85f3a4c7d8f46f7e5503b17f8577eb96b246
Jhessyjhordon <jhessyismael@gmail.com>
2023-01-24 14:27:53 +0100

    i add icons in the home page using material_icons and svg_icons

--- site-vitrine/src/app/home/home.component.ts
+++ site-vitrine/src/app/home/home.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  // add SVG icons
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'thumbs-up',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/thumb_up.svg'));
+  }
+
 
 }
